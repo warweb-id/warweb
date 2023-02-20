0b1a0462cad39f1d0e2304a765b77bdcbc8cd88c
fanyakbar <fanyakbar8@gmail.com>
2023-02-20 20:51:38 +0700

    membuat website toko aki rhg battery

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika menu diklik
+document.querySelector('#menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+// klik diluar sidebar untuk keluar menu
+const menu = document.querySelector('#menu');
+document.addEventListener('click', function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
